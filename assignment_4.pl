165cebb383ab2890d08cfd6017fcfb32bf9e5a85
kiq2908 <98726787+kiq2908@users.noreply.github.com>
2025-04-10 23:03:11 -0400

    Update assignment_4.pl

--- assignment_4.pl
+++ assignment_4.pl
@@ -6,6 +6,20 @@
 0.01::faulty_thermostat.
 0.01::power_failure.
 
+% Component behavior rules
+motion_detected(Room) :-
+    \+ faulty_motion_sensor(Room),
+    movement(Room).
+
+light_on :-
+    \+ faulty_light_sensor,
+    light_switch_on.
+
+heating_on :-
+    \+ faulty_thermostat,
+    \+ power_failure.
+
+
 % Rules defining observations
 motion_suspect(Room) :-
     faulty_motion_sensor(Room),
@@ -34,7 +48,10 @@ evidence(light_on, false).
 
 % Queries
 query(faulty_motion_sensor(living_room)).
+query(faulty_motion_sensor(hallway)).
 query(power_failure).
 query(faulty_light_sensor).
 query(faulty_temp_sensor).
 query(faulty_thermostat).
+query(heating_problem).
+query(temp_reading_unreliable).
